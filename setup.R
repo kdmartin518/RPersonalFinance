b92d51c7ee120c2d4fd8b54308f4dae3efc47fb0
Kevin <kdmartin94@pm.me>
2022-11-16 18:01:16 -0800

    Refactored directories

---
+++ setup.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(tidyr)
+library(lubridate) 
+library(ggplot2)
+
+source('source/constants.R')
+source('source/importer_functions.R')
+source('source/helper_functions.R')
+source('source/user_functions.R')
+
+
+
